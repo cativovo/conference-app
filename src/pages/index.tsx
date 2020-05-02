7e91c3dd031c9c661552af23d957d644c7b8ede2
cativovo <cativomarkleo@gmail.com>
2020-05-02 19:04:47 +0800

    added querying of talks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,39 @@
-import React from "react";
-import { NextPage } from "next";
+import gql from 'graphql-tag';
+import { NextPage } from 'next';
+import React, { useState } from 'react';
+import { ListTalksQuery } from '../API';
+import { listTalks as ListTalks } from '../graphql/queries';
+import { API } from '../utils';
 
-const Home: NextPage = () => <h1> Home</h1>;
+type HomeProps = {
+  listTalks: ListTalksQuery['listTalks'];
+};
+
+const Home: NextPage<HomeProps, HomeProps> = ({ listTalks }) => {
+  const [talks, setTalks] = useState(listTalks?.items);
+  return (
+    <div>
+      {!talks && <h1>no talks</h1>}
+      {talks && talks.map((talk) => talk && <p key={talk.id}>{talk.name}</p>)}
+    </div>
+  );
+};
+
+Home.getInitialProps = async () => {
+  const props: HomeProps = {
+    listTalks: null,
+  };
+  try {
+    const { data } = await API.query<ListTalksQuery>({
+      query: gql(ListTalks),
+    });
+
+    props.listTalks = data.listTalks;
+  } catch (error) {
+    console.error('error fetching talks');
+    console.error(error);
+  }
+  return props;
+};
 
 export default Home;
--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,6 +1,20 @@
-import { AWSAppSyncClient, AWSAppSyncClientOptions } from "aws-appsync";
-import config from "../aws-exports";
+import { Auth } from 'aws-amplify';
+import { AWSAppSyncClient } from 'aws-appsync';
+import fetch from 'isomorphic-unfetch';
+import awsExports from '../aws-exports';
+
+(global as any).fetch = fetch;
 
 export const API = new AWSAppSyncClient(
-  (config as unknown) as AWSAppSyncClientOptions
+  {
+    disableOffline: true,
+    url: awsExports.aws_appsync_graphqlEndpoint,
+    region: awsExports.aws_appsync_region,
+    auth: {
+      type: awsExports.aws_appsync_authenticationType as any,
+      apiKey: awsExports.aws_appsync_apiKey,
+    },
+    complexObjectsCredentials: () => Auth.currentCredentials(),
+  },
+  { ssrMode: true }
 );
