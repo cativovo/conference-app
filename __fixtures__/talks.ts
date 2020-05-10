ba52d556803f0ce87aa2d21c35437a4b5fe661b3
cativovo <cativomarkleo@gmail.com>
2020-05-10 10:15:43 +0800

    added fixture

---
+++ __fixtures__/talks.ts
@@ -0,0 +1,20 @@
+export const talks = [
+  {
+    __typename: 'Talk',
+    id: '1',
+    clientId: null,
+    name: 'GraphQL in 2020',
+    description: 'Using GraphQL in 2020',
+    speakerName: 'John Doe',
+    speakerBio: 'I love GraphQL',
+  },
+  {
+    __typename: 'Talk',
+    id: '2',
+    clientId: null,
+    name: 'How to cook pancit canton',
+    description: 'Cooking perfect pancit canton',
+    speakerName: 'Juan Delacruz',
+    speakerBio: 'I love pancit canton',
+  },
+];
